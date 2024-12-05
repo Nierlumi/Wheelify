a18f62b331764396978554542195430266c954e1
Jonathan <jonathankaparang9@gmail.com>
2024-12-05 19:04:04 +0700

    Menambahkan intent untuk gallery dan menambahkan logika untuk menyimpan gambar di Bundle

--- app/src/main/java/com/example/wheelify/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/example/wheelify/ui/scan/ScanFragment.kt
@@ -1,13 +1,19 @@
 package com.example.wheelify.ui.scan
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.wheelify.databinding.FragmentScanBinding
 
+@Suppress("DEPRECATION")
 class ScanFragment : Fragment() {
 
     private var _binding: FragmentScanBinding? = null
@@ -15,6 +21,7 @@ class ScanFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private var currentImageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,16 +29,60 @@ class ScanFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val scanViewModel =
-            ViewModelProvider(this).get(ScanViewModel::class.java)
+            ViewModelProvider(this)[ScanViewModel::class.java]
 
         _binding = FragmentScanBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        currentImageUri = savedInstanceState?.getParcelable(KEY_IMAGE_URI)
+        currentImageUri?.let {
+            showImage()
+        }
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        currentImageUri?.let {
+            outState.putParcelable(KEY_IMAGE_URI, it)
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Toast.makeText(requireContext(), "Tidak ada media yang dipilih", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object{
+        private const val KEY_IMAGE_URI = "key_image_uri"
+
     }
 }
